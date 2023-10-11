5ca9c2f47b54b15769d1f4cd49e8f495383053f3
yuyumou <144127852+yuyumou@users.noreply.github.com>
2023-10-11 19:27:04 +0800

    Update client.cpp
    myself

--- lab1/client.cpp
+++ lab1/client.cpp
@@ -1,81 +1,69 @@
 #include <iostream>
 #include <winsock2.h>
-#include<time.h>
-#include<windows.h>
-#include<cstring>
-#pragma comment(lib, "ws2_32.lib")
+
 using namespace std;
-SOCKET socket_client1;
-SOCKADDR_IN addr_serve1, addr_client1;
 
-const int buf_size = 2048;//缓冲区大小常量
-char send_buf1[buf_size];//发送缓冲区
-char receive_buf1[buf_size];//接受缓冲区
-char input_buf1[buf_size];
-
+#pragma comment(lib, "ws2_32.lib")
 
 int main() {
-    int FIRST_JUDGE;
     WSADATA wsaData;
-    FIRST_JUDGE = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    //指定版本wVersionRequested=MAKEWORD(a,b)。MAKEWORD()是一个宏把两个数字组合成一个WORD，无符号的短整型
-    if (FIRST_JUDGE != 0) {
-        //等于没有链接成功，失败了。
-        return 0;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "初始化失败" << endl;
+        return 1;
     }
 
-    cout << "---------------------这里是客户端-------------------" << endl << endl << endl << endl;
-
-    //创建socket。这里我们使用流式socket。
-    socket_client1 = socket(AF_INET, SOCK_STREAM, 0);
-
-    //初始化客户端地址
-    addr_client1.sin_addr.s_addr = inet_addr("127.0.0.1");//把我们本机的地址转换成网络字节二进制值序
-    addr_client1.sin_family = AF_INET;//使用ipv4
-    addr_client1.sin_port = htons(8000);//转换函数，也是转换成网络字节序。
-
-    //初始化地址
-    addr_serve1.sin_addr.s_addr = inet_addr("192.168.101.1");//把我们本机的地址转换成网络字节二进制值序
-    addr_serve1.sin_family = AF_INET;//使用ipv4
-    addr_serve1.sin_port = htons(8000);//转换函数，也是转换成网络字节序。
-    
-
-    if (connect(socket_client1, (SOCKADDR*)&addr_serve1, sizeof(addr_serve1)) != SOCKET_ERROR)
-    {
-        strcpy(send_buf1, "客户端连接成功");
-        send(socket_client1, send_buf1, 2048, 0);
-    }else {
-        //连接失败
-        return 0;
+    // 创建套接字
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cout << "socket创建失败" << endl;
+        WSACleanup();
+        return 1;
     }
 
-    while (1) {
-        recv(socket_client1, receive_buf1, sizeof(receive_buf1), 0);
-        if (!strcmp("quit", receive_buf1))
-        {
-            cout << "服务端选择了结束聊天" << endl;
+    // 连接到服务器
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("192.168.101.1");  // 服务器的IP地址
+    serverAddr.sin_port = htons(8000);  // 服务器的端口号
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "连接失败" << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    char buffer[1024];
+    int bytesRead;
+
+    while (true) {
+        // 发送客户端消息
+        cout << "客户端: ";
+        cin.getline(buffer, sizeof(buffer));
+        send(clientSocket, buffer, strlen(buffer), 0);
+
+        if (strcmp(buffer, "quit") == 0) {
+            cout << "Client disconnected." << endl;
             break;
         }
-        time_t now_time = time(NULL);
-        tm* t_tm = localtime(&now_time);
-        cout << "服务端发的：" << receive_buf1 << "  " << "收到时间: " << asctime(t_tm);
-    
 
-        cin.getline(input_buf1, 2048, '\n');
-
-        if (!strcmp("quit", input_buf1))
-        {
-            send(socket_client1, input_buf1, 2048, 0);
-            cout << "客户端选择结束聊天" << endl;
+        // 接收服务器消息
+        bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesRead <= 0) {
             break;
         }
-        else {
-            strcpy(send_buf1, input_buf1);
-            send(socket_client1, send_buf1, 2048, 0);
+        buffer[bytesRead] = '\0';
+        cout << "服务器端: " << buffer << endl;
+
+        if (strcmp(buffer, "quit") == 0) {
+            cout << "Server disconnected." << endl;
+            break;
         }
-        Sleep(30);
     }
-    closesocket(socket_client1);
+
+    // 关闭套接字
+    closesocket(clientSocket);
     WSACleanup();
 
+    return 0;
 }
